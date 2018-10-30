698a5fe704f445be625a716511fdb1503a8f4a23
william <2444870620@qq.com>
2018-10-30 12:53:18 +0800

    Add storage userinfo file.

--- app/src/main/java/com/company/management/HomePageFragment.java
+++ app/src/main/java/com/company/management/HomePageFragment.java
@@ -1,8 +1,6 @@
 package com.company.management;
 
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,22 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.View;
 import android.widget.*;
-import android.os.Handler;
-import android.os.Message;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
-import java.util.Random;
-import com.company.management.R;
 /**
  * Created by 24448 on 2018/6/12.
  */
@@ -47,13 +32,27 @@ public class HomePageFragment extends Fragment {
         in_warehouse = (ImageView) view.findViewById(R.id.material_in_warehouse);
         material_turn_back = (ImageView) view.findViewById(R.id.material_turn_back);
         get_material = (ImageView) view.findViewById(R.id.meterial_get);
-        material_apply.setOnClickListener(new ChangeToFormList());
+        material_apply.setOnClickListener(new ChangeToFormList(R.string.material_apply));
+        in_warehouse.setOnClickListener(new ChangeToFormList(R.string.material_in_warehouse));
+        material_turn_back.setOnClickListener(new ChangeToFormList(R.string.material_turn_back));
+        get_material.setOnClickListener(new ChangeToFormList(R.string.get_matrial));
     }
-//    TODO： 完成具体的跳转
+//    TODO：需要进行测试
     class ChangeToFormList implements View.OnClickListener {
+        int  target;
+        String username;
+        public ChangeToFormList(int target) {
+            this.target = target;
+            UserInfoSaveAndRead userinfo = new UserInfoSaveAndRead();
+            username = userinfo.getUserInfo(getActivity().getApplicationContext());
+        }
         @Override
         public void onClick(View v) {
             Intent intent = new Intent();
+            Bundle bundle = new Bundle();
+            bundle.putInt("page", target);
+            bundle.putString("user", username);
+            intent.putExtras(bundle);
             intent.setClassName(getActivity().getPackageName(),
                     getActivity().getPackageName() + ".FormList");
             PackageManager packageManager = getActivity().getPackageManager();
